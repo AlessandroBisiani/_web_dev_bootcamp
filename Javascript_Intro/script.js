9da01eb48d2a2234742d59c0bc80da6fb26a8f83
alessandrobisiani <bisi90@gmail.com>
2018-12-18 16:58:55 +0100

    Javascript Intro complete.

---
+++ Javascript_Intro/script.js
@@ -0,0 +1,6 @@
+// alert("Hello from script js file.
+
+
+var username = prompt("What is your name?");
+alert("It's a pleasure, " + username);
+console.log("It really is a pleasure, you curious boy.");
