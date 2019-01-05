38f50e3b6fdb2e173d96a76a71d529aa2d24c880
alessandrobisiani <bisi90@gmail.com>
2019-01-05 23:53:22 +0100

    Node and npm module

---
+++ 12_Node/MyShop/listProducts.js
@@ -0,0 +1,6 @@
+var faker = require('faker');
+
+for(var i=0 ; i<10 ; i++){
+    faker.product();
+    console.log();
+}
---
+++ 12_Node/grader.js
@@ -0,0 +1,59 @@
+// Missing something here.. This outouts:
+// 00
+// 001
+// 0012
+// 00123
+// 001234
+// 0012345
+// 12345
+// 00
+// 001
+// 0012
+// 00123
+// 001234
+// 0012345
+// 00123456
+// 001234567
+// 0012345678
+// 00123456789
+// 123456789
+// function average(scores){
+//     var average = 0;
+//     for(score in scores){
+//         average += score;
+//         console.log(average);
+
+//     }
+//     return Math.round(average);
+// }
+
+
+// function average(scores){
+//     var average = 0;
+//     for(var i=0 ; i<scores.length ; i++){
+//         average += scores[i];
+//     }
+//     average = average/scores.length;
+//     return Math.round(average);
+// }
+
+function average(scores){
+    avg = 0;
+    scores.forEach(function(score){
+        avg += score;
+    });
+    avg = avg / scores.length;
+    return Math.round(avg);
+}
+
+
+var s = [90,98, 89, 100, 86, 94];
+var s2 = [40,56,77,82,80,54,73,63,85,49];
+console.log(average(s));
+// javascript sometimes does not like return value names to be
+// the same as those of functions. 
+// Before changing "average" to "avg", this was executing well
+// until the last line, then returning: "average not a function.".
+// Perhaps because average was the return key in that context?
+// Somehow?
+console.log(average(s2));
